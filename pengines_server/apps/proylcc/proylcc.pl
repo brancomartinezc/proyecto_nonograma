e4a46b122c23241f344d5548838944af149fde99
brancomartinezc <bmartinezcarpineti@gmail.com>
2021-05-14 18:19:03 -0300

    nuevo control de lineas

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -34,16 +34,16 @@ put(Contenido, [RowN, ColN], PistasFilas, PistasColumnas, Grilla, NewGrilla, Fil
 	% NewRow es el resultado de reemplazar lo que se que haya (_Cell) en la posición ColN de Row por Contenido.
 	(replace(Cell, ColN, _, Row, NewRow), Cell == Contenido ; replace(_Cell, ColN, Contenido, Row, NewRow)),
 
-	satisfaceFila(RowN, PistasFilas, NewGrilla, FilaSat),
+	satisface(RowN, PistasFilas, NewGrilla, FilaSat),
 	% transpose/2 para rotar la grilla y asi las columnas se vuelven arreglos horizontales
 	transpose(NewGrilla, GrillaRotada),
-	satisfaceFila(ColN, PistasColumnas, GrillaRotada, ColSat), /*writeln(FilaSat),writeln(ColSat),*/ writeln("").
+	satisface(ColN, PistasColumnas, GrillaRotada, ColSat), /*writeln(FilaSat),writeln(ColSat),*/ writeln("").
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Dada una lista de strings encuentra el string que es la concatenacion de todos los elementos de la lista
 % reemplazando _ y X por un espacio en blanco
-srtListConcat([], "").
+/*srtListConcat([], "").
 srtListConcat([H | T], String) :-
 	srtListConcat(T, Rest),
 	% Si H es un _ o "X" reemplazar por " "
@@ -52,39 +52,50 @@ srtListConcat([H | T], String) :-
 		;
 	% En este caso H es "#"
 	string_concat(H, Rest, String)).
-
+*/
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 /* extraerGrupos -> L:string, Grupos:[string]
 Dada una fila "# ### " encuentra Grupos=["#", "###"] 
 Split por " ", filtrar los elementos que no sean "" */
-extraerGrupos(L, Grupos) :-
+/*extraerGrupos(L, Grupos) :-
 	split_string(L, " ", "", G),
-	findall(X, (member(X, G), X \= ""), Grupos).
+	findall(X, (member(X, G), X \= ""), Grupos).*/
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-/*
-% Dado el numero de fila se la busca entre todas las PistasFilas.
-obtenerPistasFila(0,[H|T], H).
-obtenerPistasFila(N, [H|T], PF):- 
-	N1 is N-1, 
-	obtenerGrupoPistas(N1, PF).
-*/
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%ejemplo: [#,_,X,#,#,] --> [#,x,x,#,#]
+prepararLista([],[]).
+prepararLista([H|T],[x|R]):-
+	(var(H) ; H == "X"),
+    prepararLista(T,R), !.
+prepararLista([H|T],[H|R]):-
+    prepararLista(T,R), !.
+
+%ejemplo: [#,x,x,#,#] --> [[#],[#,#]]
+split(L,P,R):- split(L,P,[],R), !.
+split([],_,[],[]).
+split([],_,S,[S]) :- S \= [].
+split([P|T],P,[],R) :- split(T,P,[],R).
+split([P|T],P,L,[L|R]) :- L \= [], split(T,P,[],R).
+split([H|T],P,L,R) :- H \= P, append(L, [H], L2), split(T,P,L2,R).
 
 %Controla que las pistas de una fila y los grupos resultantes de las jugadas del usuario sean igual
 %por ejemplo, para [1,4] el grupos ["#"","##"] es incorrecto pero ["#"","####"] si es correcto.
 %verificarNumeros(PistasFila, Grupos)
-verificarNumeros([],[]).
+/*verificarNumeros([],[]).
 verificarNumeros([P|Ps], [G|Gs]):-
-	string_length(G, Length),
+	atom_length(G, P),
 	writeln("P:"), % DEBUG
 	writeln(P), % DEBUG
-	writeln("Lenght:"), % DEBUG
-	writeln(Length), % DEBUG
-	P = Length, /* !,*/ % este cut era por un trace, pero no se necesita
-	verificarNumeros(Ps,Gs).
+	verificarNumeros(Ps,Gs).*/
+
+verificarSolucion([],[]).
+verificarSolucion([P|Ps], [G|Gs]):-
+	length(G,P),
+	writeln("P:"), % DEBUG
+	writeln(P), % DEBUG
+	verificarSolucion(Ps,Gs).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -101,14 +112,13 @@ verificarNumeros([P|Ps], [G|Gs]):-
 		resuelto y te indican si pintaste un cuadro que no esta pintado en la grilla resuelta)
 */
 %Row, RowString, PFila, Grupos
-satisfaceFila(RowN, PistasFilas, GrillaFilas, FilaSat) :- 
+/*satisfaceFila(RowN, PistasFilas, GrillaFilas, FilaSat) :- 
 	(	nth0(RowN, GrillaFilas, Row),    % obtener la RowN-esima fila
 		srtListConcat(Row, RowString),   % convertirla en un string
-		extraerGrupos(RowString, Grupos),
+		extraerGrupos(Row, Grupos),
 		writeln("Grupos: "), % DEBUG
 		writeln(Grupos), % DEBUG
 		
-		% obtenerPistasFila(RowN, PistasFilas, PFila),
 		nth0(RowN, PistasFilas, PFila),
 		writeln("Pistas: "), % DEBUG
 		writeln(PFila), % DEBUG
@@ -117,6 +127,19 @@ satisfaceFila(RowN, PistasFilas, GrillaFilas, FilaSat) :-
 		% FilaSat = 1 si length(Grupos) == length(PistasFilas) y coinciden longitud de grupos con cada pista
 		verificarNumeros(PFila, Grupos), % Esto verifica esa ultima propiedad
 		writeln("fue true"), % DEBUG
+		FilaSat = 1
+	), !;FilaSat = 0.*/
+
+satisface(RowN, PistasFilas, GrillaFilas, FilaSat) :- 
+	(	nth0(RowN, GrillaFilas, Row),    % obtener la RowN-esima fila de la grilla
+		nth0(RowN, PistasFilas, PFila), % obtener la RowN-esima fila de pistas
+		prepararLista(Row,FilaPreparada),
+		split(FilaPreparada,x,Grupos),
+		writeln("Grupos: "), % DEBUG
+		writeln(Grupos), % DEBUG
+		writeln("Pistas: "), % DEBUG
+		writeln(PFila), % DEBUG
+		verificarSolucion(PFila, Grupos),
 		FilaSat = 1
 	), !;FilaSat = 0.
 
