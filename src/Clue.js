4567d9ae1b1c23561439fa581f90fc6c35bf4c65
superscalar <65932360+superscalar@users.noreply.github.com>
2021-05-15 19:14:00 -0300

    Pintado de pistas persistente entre jugadas

--- src/Clue.js
+++ src/Clue.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
 class Clue extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          satisfies: false
+        };
+    }
+
     render() {
+        if (this.props.sat && !this.state.satisfies) {
+            // Si esta pista fue satisfecha en una jugada y era falsa antes, pintar de verde
+            this.setState({satisfies: this.props.sat});
+        } else if (!this.props.sat && this.props.satisfies) {
+            // Si esta pista ya estaba pintada y por una jugada dejo de satisfacerse, despintarla
+            this.setState({satisfies: this.props.sat});
+        }
+
         const clue = this.props.clue;
-        const sat = this.props.sat;
+        const sat = this.state.satisfies;
         return (
             <div className={`clue ${sat ? "satisfies" : ""}`} >
                 {clue.map((num, i) =>
